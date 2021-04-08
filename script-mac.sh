aefaf57b3031ef1f413ceb371ba56810b2ede308
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-04-08 09:57:18 +0200

    Update script-mac.sh to automatically install all the dependencies of blf

--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,28 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+
+# Download
+curl -LO https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+# Install with default options
+bash ./Miniforge3-Linux-x86_64.sh -b
+
+~/miniforge3/condabin/conda init
+conda config --set auto_activate_base false
+conda create -n robotologyenv
+conda activate robotologyenv
+
+conda install cmake compilers make ninja pkg-config
+conda install -c robotology idyntree yarp libmatio matio-cpp lie-group-controllers eigen qhull casadi cppad spdlog catch2
+
+cd ~
+git clone https://github.com/artivis/manif.git
+cd manif
+git checkout c8b4f16ea4d64e8d145aaf50bdbe31c756edb5b2
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} ..
+cmake --build . --config Release --target install
+
+cd ~
+git clone https://github.com/dic-iit/bipedal-locomotion-framework.git
