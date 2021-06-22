bd3723476cd888c1ae5ef9ec656799554be0efba
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-06-22 15:29:14 +0200

    Update script-mac.sh

--- script-mac.sh
+++ script-mac.sh
@@ -23,16 +23,7 @@ conda create -n robotologyenv
 conda activate robotologyenv
 
 conda install cmake compilers make ninja pkg-config
-conda install -c robotology idyntree yarp libmatio matio-cpp lie-group-controllers eigen qhull casadi cppad spdlog catch2
-
-cd ~
-git clone https://github.com/artivis/manif.git
-cd manif
-git checkout c8b4f16ea4d64e8d145aaf50bdbe31c756edb5b2
-mkdir build
-cd build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} ..
-cmake --build . --config Release --target install
+conda install -c robotology idyntree yarp libmatio matio-cpp lie-group-controllers eigen qhull casadi cppad spdlog catch2 manif manifpy
 
 cd ~
 git clone https://github.com/dic-iit/bipedal-locomotion-framework.git
