7ff92d7d6e45dd19a46ec78b22627c4de2a8b865
Giulio Romualdi <giulio.romualdi@gmail.com>
2022-05-06 10:34:52 +0200

    Update script-linux.sh

--- script-linux.sh
+++ script-linux.sh
@@ -1,7 +1,3 @@
-# disable spotlight indexing
-sudo mdutil -i off -a
-echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
-
 # remove conda
 conda init --reverse
 sudo rm -rf /usr/local/miniconda
